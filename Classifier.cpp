6031590f91d3a7b5d22b1451acf399ea04c6bc7c
berak <px1704@web.de>
2014-09-18 00:56:20 +0200

    clean up

--- Classifier.cpp
+++ Classifier.cpp
@@ -1,3 +1,6 @@
+#include <set>
+using namespace std;
+
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/ml.hpp>
@@ -187,6 +190,136 @@ public:
 };
 
 
+// 
+// ref impl of eigen / fisher faces
+//
+class ClassifierEigen : public TextureFeature::Classifier
+{
+protected:
+    int _num_components;
+    double _threshold;
+    vector<Mat> _projections;
+    Mat _labels;
+    Mat _eigenvectors;
+    Mat _mean;
+
+public:
+
+    ClassifierEigen(int num_components = 0, double threshold = DBL_MAX) 
+        : _num_components(num_components)
+        , _threshold(threshold) 
+    {}
+
+    Mat project(const Mat& src) const
+    {
+        Mat X, Y;
+        src.convertTo(X, _eigenvectors.type());
+        for(int i=0; i<src.rows; i++) 
+        {
+            Mat r_i = X.row(i);
+            subtract(r_i, _mean.reshape(1,1), r_i);
+        }
+        gemm(X, _eigenvectors, 1.0, Mat(), 0.0, Y);
+        return Y;
+    }
+
+    //Mat reconstruct(const Mat& src) const
+    //{
+    //    Mat X, Y;
+    //    src.convertTo(Y, _eigenvectors.type());
+    //    gemm(Y, _eigenvectors, 1.0, Mat(), 0.0, X, GEMM_2_T);
+    //    for(int i=0; i<src.rows; i++) 
+    //    {
+    //        Mat r_i = X.row(i);
+    //        add(r_i, _mean.reshape(1,1), r_i);
+    //    }
+    //    return X;
+    //}
+
+
+    void save_projections(const Mat& data) 
+    {
+        _projections.clear();
+        for(int i=0; i<data.rows; i++) 
+        {
+            _projections.push_back(project(data.row(i)));
+        }
+    }
+
+    virtual int train(const Mat & data, const Mat & labels) 
+    {
+        if((_num_components <= 0) || (_num_components > data.rows))
+            _num_components = data.rows;
+
+        PCA pca(data, Mat(), cv::PCA::DATA_AS_ROW, _num_components);
+
+        _labels = labels;
+        _mean   = pca.mean.reshape(1,1);
+        transpose(pca.eigenvectors, _eigenvectors);
+        save_projections(data);
+        return 1;
+    }
+
+    virtual int predict(const cv::Mat &testFeature, cv::Mat &results) const
+    {
+        Mat q = project(testFeature.reshape(1,1));
+        double minDist = DBL_MAX;
+        int minClass = -1;
+        int minId=-1;
+        for(size_t sampleIdx = 0; sampleIdx < _projections.size(); sampleIdx++) 
+        {
+            double dist = norm(_projections[sampleIdx], q, NORM_L2);
+            if((dist < minDist) && (dist < _threshold))
+            {
+                minId    = sampleIdx;
+                minDist  = dist;
+                minClass = _labels.at<int>((int)sampleIdx);
+            }
+        }
+        results.push_back(float(minClass));
+        results.push_back(float(minDist));
+        results.push_back(float(minId));
+        return 3;
+    }
+};
+
+class ClassifierFisher : public ClassifierEigen
+{
+public:
+
+    ClassifierFisher(int num_components = 0, double threshold = DBL_MAX) 
+        : ClassifierEigen(num_components, _threshold) 
+    {}
+
+    int unique(const Mat & labels) const 
+    {
+        set<int> set_elems;
+        for (size_t i=0; i<labels.total(); ++i)
+            set_elems.insert(labels.at<int>(i));
+        return set_elems.size();
+    }
+
+    virtual int train(const Mat & data, const Mat & labels)
+    {
+        int N = data.rows;
+        int C = unique(labels);
+        if((_num_components <= 0) || (_num_components > (C-1)))
+            _num_components = (C-1);
+
+        PCA pca(data, Mat(), cv::PCA::DATA_AS_ROW, (N-C));
+        LDA lda(pca.project(data),labels, _num_components);
+
+        Mat leigen; // hmm, it's new, that i have to convert.
+        lda.eigenvectors().convertTo(leigen, pca.eigenvectors.type());
+        gemm(pca.eigenvectors, leigen, 1.0, Mat(), 0.0, _eigenvectors, GEMM_1_T);
+
+        _labels = labels;
+        _mean   = pca.mean.reshape(1,1);
+        save_projections(data);
+        return 1;
+    }
+};
+
 
 
 
@@ -206,12 +339,9 @@ cv::Ptr<TextureFeature::Classifier> createClassifierKNN(int k=1)
 cv::Ptr<TextureFeature::Classifier> createClassifierSVM(double degree = 0.5,double gamma = 0.8,double coef0 = 0,double C = 0.99, double nu = 0.2, double p = 0.5)
 { return makePtr<Svm>(degree, gamma, coef0, C, nu, p); }
 
-//cv::Ptr<TextureFeature::Classifier> createClassifierXX(String s)
-//{ 
-//    //cv::Ptr<ml::StatModel> m = ml::StatModel::create(s);
-//    //cv::Ptr<ml::StatModel> m = Algorithm::create<ml::StatModel>(s);
-//    //cv::Ptr<ml::StatModel> m = ml::LogisticRegression::create();
-//    cv::Ptr<ml::StatModel> m = ml::KNearest::create();
-//    return makePtr<ClassifierXX>(m); 
-//}
-//
+cv::Ptr<TextureFeature::Classifier> createClassifierEigen()
+{ return makePtr<ClassifierEigen>(); }
+
+cv::Ptr<TextureFeature::Classifier> createClassifierFisher()
+{ return makePtr<ClassifierFisher>(); }
+
--- extractDB.cpp
+++ extractDB.cpp
@@ -1,5 +1,6 @@
 #include "extractDB.h"
 
+#include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -15,7 +16,66 @@ using namespace std;
 
 
 #include "TextureFeature.h"
-#include "tan_triggs.h"
+
+
+//
+// taken from : https://github.com/bytefish/opencv/blob/master/misc/tan_triggs.cpp
+//
+Mat tan_triggs_preprocessing(InputArray src, float alpha=0.1, float tau=10.0, float gamma=0.2, int sigma0=1, int sigma1=2) 
+{
+    // Convert to floating point:
+    Mat X = src.getMat();
+    X.convertTo(X, CV_32FC1);
+    // Start preprocessing:
+    Mat I;
+    pow(X, gamma, I);
+    // Calculate the DOG Image:
+    {
+        Mat gaussian0, gaussian1;
+        // Kernel Size:
+        int kernel_sz0 = (3*sigma0);
+        int kernel_sz1 = (3*sigma1);
+        // Make them odd for OpenCV:
+        kernel_sz0 += ((kernel_sz0 % 2) == 0) ? 1 : 0;
+        kernel_sz1 += ((kernel_sz1 % 2) == 0) ? 1 : 0;
+        GaussianBlur(I, gaussian0, Size(kernel_sz0,kernel_sz0), sigma0, sigma0, BORDER_CONSTANT);
+        GaussianBlur(I, gaussian1, Size(kernel_sz1,kernel_sz1), sigma1, sigma1, BORDER_CONSTANT);
+        subtract(gaussian0, gaussian1, I);
+    }
+
+    {
+        double meanI = 0.0;
+        {
+            Mat tmp;
+            pow(abs(I), alpha, tmp);
+            meanI = mean(tmp).val[0];
+
+        }
+        I = I / pow(meanI, 1.0/alpha);
+    }
+
+    {
+        double meanI = 0.0;
+        {
+            Mat tmp;
+            pow(min(abs(I), tau), alpha, tmp);
+            meanI = mean(tmp).val[0];
+        }
+        I = I / pow(meanI, 1.0/alpha);
+    }
+
+    // Squash into the tanh:
+    {
+        for(int r = 0; r < I.rows; r++) {
+            for(int c = 0; c < I.cols; c++) {
+                I.at<float>(r,c) = tanh(I.at<float>(r,c) / tau);
+            }
+        }
+        I = tau * I;
+    }
+    return I;
+}
+
 
 
 //
