ec0dd17586977b738f3f1c0134b98bf70dc49fa1
berak <px1704@web.de>
2014-11-26 09:03:32 +0100

    cleanup

--- main.cpp
+++ main.cpp
@@ -17,18 +17,100 @@ using namespace std;
 
 
 #include "TextureFeature.h"
-#include "extractDB.h"
+#include "Preprocessor.h"
 
 
 using TextureFeature::Extractor;
 using TextureFeature::Classifier;
 
 bool debug = false;
-
-double ct(int64 t) {    return double(t) / cv::getTickFrequency(); }
-
 RNG rng(getTickCount());
 
+double ct(int64 t) 
+{
+    return double(t) / cv::getTickFrequency(); 
+}
+
+
+//
+// read a 'path <blank> label' list
+//
+int readtxt( const char * fname, std::vector<std::string> & names, std::vector<int> & labels, size_t maxim  ) 
+{
+    int maxid=-1;
+    std::ifstream in(fname);
+    while( in.good() && !in.eof() )
+    {
+        std::string path ;
+        in >> path;
+        names.push_back(path);
+
+        int label;
+        in >> label;
+        labels.push_back(label);
+
+        maxid=std::max(maxid,label);
+        if ( labels.size() >= maxim ) 
+            break;
+    }
+    return maxid;
+}
+
+
+//
+// imglists per person.
+//  no really, you can't just draw a random probability set from a set of multiple classes and call it a day ...
+//
+void setupPersons( const vector<int> & labels, vector<vector<int>> & persons )
+{
+    // find out which index belongs to which person
+    //
+    persons.resize(1);
+    int prvid=0;
+    for ( size_t j=0; j<labels.size(); j++ )
+    {
+        int id = labels[j];
+        if (prvid!=id)
+        {
+            persons.push_back(vector<int>());
+            prvid=id;
+        }
+        persons.back().push_back(j);
+    }
+}
+
+int extractDB(const string &txtfile, vector<Mat> & images, Mat & labels, int preproc, int precrop, int maxim, int fixed_size)
+{
+    // read face db
+    vector<string> vec;
+    vector<int> vlabels; 
+    int nsubjects = 1 + readtxt(txtfile.c_str(), vec, vlabels, maxim);
+
+    Preprocessor pre(preproc,precrop);
+
+    //
+    // read the images, 
+    //   correct labels if empty images are skipped
+    //   also apply preprocessing,
+    //
+    int load_flag = preproc==-1 ? 1 : 0;
+    for ( size_t i=0; i<vec.size(); i++ )
+    {
+        Mat m1 = imread(vec[i], load_flag);
+        if ( m1.empty() )
+            continue;
+
+        Mat m2;
+        resize(m1, m2, Size(fixed_size,fixed_size));
+
+        Mat m3 = pre.process(m2);
+
+        images.push_back(m3);
+        labels.push_back(vlabels[i]);
+        //if ( i%33==0) imshow("i",mm), waitKey(0);
+    }
+    return nsubjects;
+}
 
 int crossfoldData( Ptr<Extractor> ext,
                     Mat & trainFeatures,
@@ -160,7 +242,7 @@ int main(int argc, const char *argv[])
     if ( argc>5 ) debug = atoi(argv[5])!=0;
 
     
-    extractDB(db_path, images, labels, preproc, 500, 120);
+    extractDB(db_path, images, labels, preproc, 0, 500, 120);
 
     // per person id lookup
     vector<vector<int>> persons;
