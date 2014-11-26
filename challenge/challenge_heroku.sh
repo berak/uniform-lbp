48e5a83a385461eaf59d758e4af36ee0ea2cf8db
berak <px1704@web.de>
2014-11-26 15:54:35 +0100

    heroku

--- challenge/challenge_heroku.sh
+++ challenge/challenge_heroku.sh
@@ -1,4 +1,11 @@
 git clone https://github.com/berak/uniform-lbp
 cd uniform-lbp/challenge
+curl http://vis-www.cs.umass.edu/lfw/lfw-deepfunneled.tgz > lfw.tgz
+tar -xf lfw.tgz
+cd lfw-deepfunneled
+curl http://vis-www.cs.umass.edu/lfw/pairs.txt > pairs.txt
+curl http://vis-www.cs.umass.edu/lfw/pairsDevTrain.txt > pairsDevTrain.txt
+cd ..
+rm -f  lfw.tgz
 
-make
+bash makefile
